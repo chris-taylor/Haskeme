8bc00ee0c94fcf708a0c3e1c3fb75b8f48c66894
Chris Taylor <crntaylor@gmail.com>
2012-04-11 23:14:18 +0100

    A few more parsers

--- src/scm.hs
+++ src/scm.hs
@@ -1,7 +1,26 @@
 module Main where
 
 import System.Environment
+import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
+
+-- Data definitions
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+-- Main
+
+main :: IO ()
+main = do
+    args <- getArgs
+    putStrLn (readExpr (args !! 0))
+
+-- Parsers
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -9,12 +28,29 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first:rest
+    return $ case atom of
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+-- Functions
+
 readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
-
-main :: IO ()
-main = do
-    args <- getArgs
-    putStrLn (readExpr (args !! 0))
