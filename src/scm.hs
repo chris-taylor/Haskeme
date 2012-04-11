489494fea646306936784e9bfb338d4a0f47faa3
Chris Taylor <crntaylor@gmail.com>
2012-04-11 23:06:08 +0100

    Parser

--- src/scm.hs
+++ src/scm.hs
@@ -1,4 +1,4 @@
-module Scheme where
+module Main where
 
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
@@ -6,8 +6,11 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
 
