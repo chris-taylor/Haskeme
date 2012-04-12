f60140d0fb6ff110c8caf9a503c26679f30d2fc0
Chris Taylor <crntaylor@gmail.com>
2012-04-12 23:19:36 +0100

    Beginnings of eval

--- src/scm.hs
+++ src/scm.hs
@@ -36,20 +36,48 @@ instance Show LispVal where
 -- Main
 
 main :: IO ()
-main = do
-    args <- getArgs
-    putStrLn (readExpr (args !! 0))
+main = getArgs >>= print . eval . readExpr . head
 
 -- Functions
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found: " ++ show val
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val
 
 -- Evaluation
 
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Char _)   = val
+eval val@(Number _) = val
+eval val@(Bool _)   = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args))  = apply func (map eval args)
 
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) (lookup func primitives)
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [ ("+", numericBinop (+))
+             , ("-", numericBinop (-))
+             , ("*", numericBinop (*))
+             , ("/", numericBinop div)
+             , ("mod", numericBinop mod)
+             , ("quotient", numericBinop quot)
+             , ("remainder", numericBinop rem) ]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op = Number . foldl1 op . map unpackNum
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = if null parsed
+                        then 0
+                        else fst $ parsed !! 0
+                        where parsed = reads n
+unpackNum (List [n]) = unpackNum n
+unpackNum _          = 0
 
 -- Parsers
 
