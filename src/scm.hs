e50a1893b86f1abc3eb52116ec39682d8f173a0f
Chris Taylor <crntaylor@gmail.com>
2012-04-13 15:02:29 +0200

    added conditionals

--- src/scm.hs
+++ src/scm.hs
@@ -52,6 +52,12 @@ eval val@(Char _)   = return val
 eval val@(Number _) = return val
 eval val@(Bool _)   = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", predicate, conseq, alt]) = do
+    result <- eval predicate
+    case result of
+        Bool True  -> eval conseq
+        Bool False -> eval alt
+        notBool    -> throwError $ TypeMismatch "boolean" notBool
 eval (List (Atom func : args))  = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
