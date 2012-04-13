389179fc36f9ff166da1d5ac754f8faee59cc53b
Chris Taylor <crntaylor@gmail.com>
2012-04-13 18:20:35 +0100

    Moved data structures to separate file@

--- src/Main.hs
+++ src/Main.hs
@@ -10,31 +10,7 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 import Numeric (readInt)
 import Char (digitToInt)
 
--- Data definitions
-
-data LispVal = Atom String
-             | List [LispVal]
-             | DottedList [LispVal] LispVal
-             | Number Integer
-             | Char Char
-             | String String
-             | Bool Bool
-
-showVal :: LispVal -> String
-showVal (String contents) = "\"" ++ contents ++ "\""
-showVal (Char char) = ['#','\\',char]
-showVal (Atom name) = name
-showVal (Number contents) = show contents
-showVal (Bool True) = "#t"
-showVal (Bool False) = "#f"
-showVal (List contents) = "(" ++ unwordsList contents ++ ")"
-showVal (DottedList hd tl) = "(" ++ unwordsList hd ++ " . " ++ showVal tl ++ ")"
-
-unwordsList :: [LispVal] -> String
-unwordsList = unwords . map showVal
-
-instance Show LispVal where
-    show = showVal
+import LispVal
 
 -- Main
 
