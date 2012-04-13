26280f4ef14ef3c276c44335b55a2a50fbd94dac
Chris Taylor <crntaylor@gmail.com>
2012-04-13 18:20:07 +0100

    Moved data structures to separate file

---
+++ src/LispVal.hs
@@ -0,0 +1,25 @@
+module LispVal where
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | Char Char
+             | String String
+             | Bool Bool
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Char char) = ['#','\\',char]
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList hd tl) = "(" ++ unwordsList hd ++ " . " ++ showVal tl ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+instance Show LispVal where
+    show = showVal
