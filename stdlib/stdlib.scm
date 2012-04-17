b2bb932db31fe5e6ab36f47cd99b395085f6fc45
Chris Taylor <crntaylor@gmail.com>
2012-04-17 19:05:49 +0200

    added caar, cadr etc to stdlib

--- stdlib/stdlib.scm
+++ stdlib/stdlib.scm
@@ -87,3 +87,16 @@
         (if (pred x)
             (cons x y)
             y)) '() lst))
+
+(define (caar x)
+    (car (car x)))
+
+(define (cadr x)
+    (car (cdr x)))
+
+(define (cdar x)
+    (cdr (car x)))
+
+(define (cddr x)
+    (cdr (cdr x)))
+
