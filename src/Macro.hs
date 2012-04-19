cdde4f10ec4ed6bb50000b7a60a479981a9955d3
Chris Taylor <crntaylor@gmail.com>
2012-04-19 15:32:31 +0200

    Greatly simplified macros

--- src/Macro.hs
+++ src/Macro.hs
@@ -12,52 +12,32 @@ evalMacro :: (Env, Env) -> LispVal -> IOThrowsError LispVal
 {-  Macro evaluation takes two environments: the normal environment containing
     all primitives and user definitions, and a special macro environment that
     contains only macro forms. Evaluation works as follows:
-    - If the form is a list, and the first element is 'defmacro', then create a
-      new macro and store it in the macro environment.
+    - If the form is a list, and the first element is they symbol 'macro, then
+      create a new macro and store it in the macro environment.
     - If the form is a list with an atom as the first element, then check if
       that atom has a binding in the macro environment. If it does, then apply
       the macro to the remaining elements of the list **without evaluating them
       first**, otherwise return the entire form.
+    - If the form is an atom, check if it has a binding in the macro
+      environment. If so, then quote and return the associated macro, otherwise
+      return the form.
     - If the form is anything other than a list, then return it. -}
-evalMacro (e1, e2) (List (Atom "defmacro" : List (Atom var : params) : body)) = 
-    makeNormalMacro e1 params body >>= defineVar e2 var >> return (List [])
-evalMacro (e1, e2) (List (Atom "defmacro" : DottedList (Atom var : params) varargs : body)) =
-    makeVarArgs varargs e1 params body >>= defineVar e2 var >> return (List [])
-evalMacro (_ , e2) val@(List (Atom name : args)) = do
-    env <- liftIO $ readIORef e2
+evalMacro (env, macroEnv) (List (Atom "macro" : List (Atom var : params) : body)) = 
+    makeNormalFunc env params body >>= defineVar macroEnv var >> return (List [])
+evalMacro (env, macroEnv) (List (Atom "macro" : DottedList (Atom var : params) varargs : body)) =
+    makeVarArgs varargs env params body >>= defineVar macroEnv var >> return (List [])
+evalMacro (_ , macroEnv) val@(List (Atom name : args)) = do
+    env <- liftIO $ readIORef macroEnv
     maybe (return val)
           (\macroRef -> do
             macro <- liftIO $ readIORef macroRef
-            applyMacro macro args)
+            apply macro args)
+          (lookup name env)
+evalMacro (_, macroEnv) val@(Atom name) = do
+    env <- liftIO $ readIORef macroEnv
+    maybe (return val)
+          (\macroRef -> do
+            macro <- liftIO $ readIORef macroRef
+            return $ List [Atom "quote", macro])
           (lookup name env)
 evalMacro (_, _) val = return val
-
-applyMacro :: LispVal -> [LispVal] -> IOThrowsError LispVal
-{-  The code here is identical to application of a user-defined function, except
-    that it accepts a value of type Macro instead of Func. Is it possible to
-    represent Macros as Funcs, but store them in the macro environment instead
-    of in the normal environment? -}
-applyMacro (Macro params varargs body closure) args = 
-    if num params /= num args && varargs == Nothing
-        then throwError $ NumArgs (num params) args
-        else (liftIO $ bindVars closure $ zip params args) >>= bindVarArgs varargs >>= evalBody
-    where
-        remainingArgs = drop (length params) args
-        num = toInteger . length
-        evalBody env = liftM last $ mapM (eval env) body
-        bindVarArgs arg env = case arg of
-            Just argName -> liftIO $ bindVars env [(argName, List $ remainingArgs)]
-            Nothing -> return env
-
-{-  These functions are nearly identical to makeFun, makeNormalFun etc in
-    EvalApply.hs except that they return Macro instead of Func. Can they be
-    unified? -}
-
-makeMacro :: (Monad m) => Maybe String -> Env -> [LispVal] -> [LispVal] -> m LispVal
-makeMacro varargs env params body = return $ Macro (map showVal params) varargs body env
-
-makeNormalMacro :: (Monad m) => Env -> [LispVal] -> [LispVal] -> m LispVal
-makeNormalMacro = makeMacro Nothing
-
-makeVarArgs :: (Monad m) => LispVal -> Env -> [LispVal] -> [LispVal] -> m LispVal
-makeVarArgs = makeMacro . Just . showVal
