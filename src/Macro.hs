3aef7bdda654d75164e942aaa84687eca951612c
Chris Taylor <crntaylor@gmail.com>
2012-04-20 17:42:13 +0200

    comment out everything in Macro.hs for later removal

--- src/Macro.hs
+++ src/Macro.hs
@@ -1,43 +1,43 @@
-module Macro where
+--module Macro where
 
-import Data.IORef
-import Control.Monad.Error
+--import Data.IORef
+--import Control.Monad.Error
 
-import LispVal
-import LispError
-import EvalApply
-import Variables
+--import LispVal
+--import LispError
+--import EvalApply
+--import Variables
 
-evalMacro :: (Env, Env) -> LispVal -> IOThrowsError LispVal
-{-  Macro evaluation takes two environments: the normal environment containing
-    all primitives and user definitions, and a special macro environment that
-    contains only macro forms. Evaluation works as follows:
-    - If the form is a list, and the first element is they symbol 'macro, then
-      create a new macro and store it in the macro environment.
-    - If the form is a list with an atom as the first element, then check if
-      that atom has a binding in the macro environment. If it does, then apply
-      the macro to the remaining elements of the list **without evaluating them
-      first**, otherwise return the entire form.
-    - If the form is an atom, check if it has a binding in the macro
-      environment. If so, then quote and return the associated macro, otherwise
-      return the form.
-    - If the form is anything other than a list, then return it. -}
-evalMacro (env, macroEnv) (List (Atom "macro" : List (Atom var : params) : body)) = 
-    makeNormalMacro env params body >>= defineVar macroEnv var >>= (\v -> return $ List [Atom "quote", v])
-evalMacro (env, macroEnv) (List (Atom "macro" : DottedList (Atom var : params) varargs : body)) =
-    makeVarArgsMacro varargs env params body >>= defineVar macroEnv var >>= (\v -> return $ List [Atom "quote", v])
-evalMacro (_ , macroEnv) val@(List (Atom name : args)) = do
-    env <- liftIO $ readIORef macroEnv
-    maybe (return val)
-          (\macroRef -> do
-            macro <- liftIO $ readIORef macroRef
-            apply macro args)
-          (lookup name env)
-evalMacro (_, macroEnv) val@(Atom name) = do
-    env <- liftIO $ readIORef macroEnv
-    maybe (return val)
-          (\macroRef -> do
-            macro <- liftIO $ readIORef macroRef
-            return $ List [Atom "quote", macro])
-          (lookup name env)
-evalMacro (_, _) val = return val
+--evalMacro :: (Env, Env) -> LispVal -> IOThrowsError LispVal
+--{-  Macro evaluation takes two environments: the normal environment containing
+--    all primitives and user definitions, and a special macro environment that
+--    contains only macro forms. Evaluation works as follows:
+--    - If the form is a list, and the first element is they symbol 'macro, then
+--      create a new macro and store it in the macro environment.
+--    - If the form is a list with an atom as the first element, then check if
+--      that atom has a binding in the macro environment. If it does, then apply
+--      the macro to the remaining elements of the list **without evaluating them
+--      first**, otherwise return the entire form.
+--    - If the form is an atom, check if it has a binding in the macro
+--      environment. If so, then quote and return the associated macro, otherwise
+--      return the form.
+--    - If the form is anything other than a list, then return it. -}
+----evalMacro (env, macroEnv) (List (Atom "macro" : List (Atom var : params) : body)) = 
+----    makeNormalMacro env params body >>= defineVar macroEnv var >>= (\v -> return $ List [Atom "quote", v])
+----evalMacro (env, macroEnv) (List (Atom "macro" : DottedList (Atom var : params) varargs : body)) =
+----    makeVarArgsMacro varargs env params body >>= defineVar macroEnv var >>= (\v -> return $ List [Atom "quote", v])
+----evalMacro (_ , macroEnv) val@(List (Atom name : args)) = do
+----    env <- liftIO $ readIORef macroEnv
+----    maybe (return val)
+----          (\macroRef -> do
+----            macro <- liftIO $ readIORef macroRef
+----            apply macro args)
+----          (lookup name env)
+----evalMacro (_, macroEnv) val@(Atom name) = do
+----    env <- liftIO $ readIORef macroEnv
+----    maybe (return val)
+----          (\macroRef -> do
+----            macro <- liftIO $ readIORef macroRef
+----            return $ List [Atom "quote", macro])
+----          (lookup name env)
+--evalMacro (_, _) val = return val
