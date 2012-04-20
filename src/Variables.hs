ee5570d50ac40ad64fe465eb4bf36c45be763942
Chris Taylor <crntaylor@gmail.com>
2012-04-20 15:13:21 +0200

    string, vector and hash mutation

--- src/Variables.hs
+++ src/Variables.hs
@@ -2,6 +2,7 @@ module Variables where
 
 import Data.IORef
 import Data.Array
+import qualified Data.Map as Map
 import Control.Monad.Error
 
 import LispVal
@@ -21,12 +22,16 @@ getVar envRef var = do
           (lookup var env)
 
 setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
-setVar envRef var value = do
+setVar envRef var val = do
     env <- liftIO $ readIORef envRef
     maybe (throwError $ UnboundVar "Setting an unbound variable" var)
-          (liftIO . (flip writeIORef value))
+          (liftIO . (flip writeIORef val))
           (lookup var env)
-    return value
+    return val
+
+replace :: Integer -> [a] -> a -> [a]
+replace n lst val = xs ++ val:ys where
+    (xs, _:ys) = splitAt (fromInteger n) lst
 
 setCar :: Env -> String -> LispVal -> IOThrowsError LispVal
 setCar envRef var val = do
@@ -38,12 +43,12 @@ setCar envRef var val = do
                 List (_ : cdr)          -> liftIO $ writeIORef varRef (List (val : cdr))
                 DottedList (_ : cdr) tl -> liftIO $ writeIORef varRef (DottedList (val : cdr) tl)
                 Vector arr              -> liftIO $ writeIORef varRef (Vector $ listArray bds (val : cdr)) where
-                  bds       = bounds arr
-                  (_ : cdr) = elems arr
+                    bds       = bounds arr
+                    (_ : cdr) = elems arr
                 String (_ : cdr) -> liftIO $ writeIORef varRef (case val of
-                  Char c -> String (c : cdr)
-                  _      -> List (val : map Char cdr))
-                other    -> throwError $ TypeMismatch "pair, vector" other)
+                    Char c -> String (c : cdr)
+                    _      -> List (val : map Char cdr))
+                other    -> throwError $ TypeMismatch "pair, vector, string" other)
           (lookup var env)
     return val
 
@@ -54,17 +59,59 @@ setCdr envRef var val = do
           (\varRef -> do
             oldVal <- liftIO $ readIORef varRef
             case oldVal of
-                List (car : _) -> case val of
-                    List xs          -> liftIO $ writeIORef varRef (List (car : xs))
-                    DottedList xs tl -> liftIO $ writeIORef varRef (DottedList (car : xs) tl)
-                    _                -> liftIO $ writeIORef varRef (DottedList [car] val)
-                DottedList (car : _) _ -> case val of
-                    List xs          -> liftIO $ writeIORef varRef (List (car : xs))
-                    DottedList xs tl -> liftIO $ writeIORef varRef (DottedList (car : xs) tl)
-                    _                -> liftIO $ writeIORef varRef (DottedList [car] val)
+                List (car : _)          -> liftIO $ writeIORef varRef (case val of
+                    List xs          -> List (car : xs)
+                    DottedList xs tl -> DottedList (car : xs) tl
+                    _                -> DottedList [car] val)
+                DottedList (car : _) _  -> liftIO $ writeIORef varRef (case val of
+                    List xs          -> List (car : xs)
+                    DottedList xs tl -> DottedList (car : xs) tl
+                    _                -> DottedList [car] val)
+                Vector arr              -> liftIO $ writeIORef varRef (case val of
+                    Vector arr'      -> Vector $ listArray (0,n+1) (car : cdr) where
+                        (_, n) = bounds arr'
+                        cdr    = elems arr'
+                    List xs          -> List (car : xs)
+                    DottedList xs tl -> DottedList (car : xs) tl
+                    _                -> DottedList [car] val)
+                    where car = arr ! 0
+                String (car : _)        -> liftIO $ writeIORef varRef (case val of
+                    String xs        -> String (car : xs)
+                    List xs          -> List (Char car : xs)
+                    DottedList xs tl -> DottedList (Char car : xs) tl
+                    _                -> DottedList [Char car] val)
                 notPair -> throwError $ TypeMismatch "pair" notPair)
           (lookup var env)
     return val
+
+setIndex :: Env -> String -> LispVal -> LispVal -> IOThrowsError LispVal
+setIndex envRef var index val = do
+    env <- liftIO $ readIORef envRef
+    maybe (throwError $ UnboundVar "Setting index of an unbound variable" var)
+          (\varRef -> do
+            oldVal <- liftIO $ readIORef varRef
+            case oldVal of
+                String str -> setStringIndex varRef str index val
+                Vector arr -> setVectorIndex varRef arr index val
+                Hash hash  -> setHashKey varRef hash index val
+                other -> throwError $ TypeMismatch "string" other)
+          (lookup var env)
+    return val
+
+setStringIndex :: IORef LispVal -> String -> LispVal -> LispVal -> IOThrowsError ()
+setStringIndex varRef str index val = case index of
+    Number n -> case val of
+        Char c  -> liftIO $ writeIORef varRef (String $ replace n str c)
+        notChar -> throwError $ TypeMismatch "char" notChar
+    notInt   -> throwError $ TypeMismatch "integer" notInt
+
+setVectorIndex :: IORef LispVal -> VectorType -> LispVal -> LispVal -> IOThrowsError ()
+setVectorIndex varRef arr index val = case index of
+    Number n -> liftIO $ writeIORef varRef (Vector $ arr // [(fromInteger n, val)])
+    notInt   -> throwError $ TypeMismatch "integer" notInt
+
+setHashKey :: IORef LispVal -> HashType -> LispVal -> LispVal -> IOThrowsError ()
+setHashKey varRef hash key val = liftIO $ writeIORef varRef (Hash $ Map.insert key val hash)
 
 defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
 defineVar envRef var value = do
