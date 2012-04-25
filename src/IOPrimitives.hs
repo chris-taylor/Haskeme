52e4805de396b750b6cac09de2f4113f9e1fb29d
Chris Taylor <crntaylor@gmail.com>
2012-04-25 19:20:15 +0200

    Update src/IOPrimitives.hs

--- src/IOPrimitives.hs
+++ src/IOPrimitives.hs
@@ -25,7 +25,7 @@ applyProc [func, List args] = apply func args
 applyProc (func : args)     = apply func args
 
 makePort :: IOMode -> [LispVal] -> IOThrowsError LispVal
-makePort mode [String filename] = liftM Port $ liftIO $ openFile filename mode
+makePort mode [String filename] = asIO Port $ openFile filename mode
 
 closePort :: [LispVal] -> IOThrowsError LispVal
 closePort [Port port] = liftIO $ hClose port >> (return $ Bool True)
@@ -40,14 +40,27 @@ writeProc [obj] = writeProc [obj, Port stdout]
 writeProc [obj, Port port] = liftIO $ hPrint port obj >> (return $ Bool True)
 
 readContents :: [LispVal] -> IOThrowsError LispVal
-readContents [String filename] = liftM String $ liftIO $ readFile filename
+readContents [String filename] = asIO String $ readFile filename
 
 readAll :: [LispVal] -> IOThrowsError LispVal
 readAll [String filename] = liftM List $ load filename
 
 rand :: [LispVal] -> IOThrowsError LispVal
 rand [modulus] = case modulus of
-    Number n -> liftM Number $ liftIO $ randomRIO (0, n - 1)
-    Float n  -> liftM Float $ liftIO $ randomRIO (0, n)
-    Ratio n  -> liftM (Ratio . toRational) $ liftIO $ randomRIO (0 , fromRational n :: Double)
+    Number n -> asIO Number $ randomRIO (0, n - 1)
+    Float n  -> asIO Float $ randomRIO (0, n)
+    Ratio n  -> asIO (Ratio . toRational) $ randomRIO (0 , fromRational n :: Double)
 rand badArgs = throwError $ NumArgs 1 badArgs
+
+-- Helper functions
+
+{- liftIO :: MonadIO m => IO a -> m a
+        Lifts IO values into an IO-compatible monad
+    liftM :: Monad m => (a -> b) -> (m a -> m b)
+        Converts arbitrary functions into monadic functions
+    asIO :: MonadIO m => (a -> b) -> (IO a -> m b)
+        Converts arbitrary functions into functions that map IO values to
+        arbitrary IO-compatible monads -}
+
+asIO :: (MonadIO m) => (a -> b) -> (IO a -> m b)
+asIO func = liftM func . liftIO
