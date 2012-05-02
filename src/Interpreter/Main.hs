168c17715dc769b287388c6e7bf12602eebc3996
Chris Taylor <crntaylor@gmail.com>
2012-05-02 13:59:39 +0200

    Update src/Interpreter/Main.hs

--- src/Interpreter/Main.hs
+++ src/Interpreter/Main.hs
@@ -15,6 +15,28 @@ import Language.Variables
 import Paths_haskeme
 import Data.Version (showVersion)
 
+-- Config
+
+showHeader :: IO ()
+showHeader = do
+    putStrLn $ " _               _"
+    putStrLn $ "| |_   ___  ___ | |__ ___  _____  ___"
+    putStrLn $ "|    \\/ _ \\/ __/| | // _ \\|     \\/ _ \\"
+    putStrLn $ "| || | |_| \\__ \\|   \\  __/| | | |  __/"
+    putStrLn $ "|_||_|\\__/\\|___/|_|\\_\\___/|_|_|_|\\___/"
+    putStrLn $ "   Author: github.com/chris-taylor"
+    putStrLn $ "  Version: " ++ showVersion version
+    putStrLn $ ""
+
+showMsg :: IO ()
+showMsg = putStrLn "Welcome to Haskeme! Type :q to quit.\n"
+
+defaultPrompt :: String
+defaultPrompt = "haskeme> "
+
+quitCmd :: String
+quitCmd = ":q"
+
 -- Main
 
 main :: IO ()
@@ -33,40 +55,38 @@ runRepl = do
     showHeader
     env <- primitiveBindings
     loadLibraries env
-    untilM_ (== "quit") (readPrompt "haskeme> ") (evalAndPrint env)
+    showMsg
+    untilM_ (== quitCmd) (readPrompt defaultPrompt) (handleInput env)
+
+-- IO Functions
 
 loadLibraries :: Env -> IO ()
 loadLibraries env = do
     stdlib <- getDataFileName $ if os == "mingw32"
         then "lib\\stdlib.scm"
         else "lib/stdlib.scm"
-    putStrLn $ "Loading library from " ++ stdlib
+    putStrLn $ "Loading stdlib from " ++ stdlib ++ "\n"
     evalString env $ "(load  \"" ++ (escapeBackslashes stdlib) ++ "\")"
 
--- IO Functions
+handleInput :: Env -> String -> IO ()
+handleInput env expr = case expr of
+    ""        -> return ()
+    ':' : cmd -> handleCommand env cmd
+    _         -> evalAndPrint env expr
 
-showHeader :: IO ()
-showHeader = do
-    putStrLn $ " _               _"
-    putStrLn $ "| |_   ___  ___ | |__ ___  _____  ___"
-    putStrLn $ "|    \\/ _ \\/ __/| | // _ \\|     \\/ _ \\"
-    putStrLn $ "| || | |_| \\__ \\|   \\  __/| | | |  __/"
-    putStrLn $ "|_||_|\\__/\\|___/|_|\\_\\___/|_|_|_|\\___/"
-    putStrLn $ "   Author: github.com/chris-taylor"
-    putStrLn $ "  Version: " ++ showVersion version
-
-flushStr :: String -> IO ()
-flushStr str = putStr str >> hFlush stdout
+handleCommand :: Env -> String -> IO ()
+handleCommand _ _ = return ()
 
 readPrompt :: String -> IO String
 readPrompt prompt = flushStr prompt >> getLine
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
 -- Macro expansion and evaluation
 
 evalAndPrint :: Env -> String -> IO ()
-evalAndPrint env expr = if null expr
-    then return ()
-    else evalExpr env expr >>= putStrLn
+evalAndPrint env expr = evalExpr env expr >>= putStrLn
 
 evalString :: Env -> String -> IO ()
 evalString env expr = evalExpr env expr >> return ()
@@ -100,3 +120,6 @@ replace s find repl =
     if take (length find) s == find
         then repl ++ (replace (drop (length find) s) find repl)
         else [head s] ++ (replace (tail s) find repl)
+
+lstrip :: String -> String
+lstrip = dropWhile (`elem` " \t")
