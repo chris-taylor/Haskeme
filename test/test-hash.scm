0a1b0a9a445405ae5589553100b9511c8716657e
Chris Taylor <crntaylor@gmail.com>
2012-05-13 22:26:00 +0100

    Tests for hashes

---
+++ test/test-hash.scm
@@ -0,0 +1,24 @@
+;;;; Tests for hashes
+
+(run-tests "Hash"
+
+(do
+  (def m #(a 1 b 2))
+  
+  (assert-equal (m 'a) 1  "Test map lookup (1)")
+  (assert-equal (m 'b) 2  "Test map lookup (2)")
+  
+  (= (m 'c) 3)
+  (assert-equal (m 'c) 3  "Test map lookup (3)"))
+
+(do
+  (def m #(a 1 b 2))
+  (def a (hash->alist m))
+  (assert-equal a '((a 1) (b 2))  "Test hash -> alist conversion"))
+
+(do
+  (def a '((a 1) (b 2)))
+  (def m (alist->hash a))
+  (assert-equal m #(a 1 b 2)      "Test alist -> hash conversion"))
+
+)
